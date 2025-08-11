01f082423b6fde0eb4ee9a7c6e94b6d117cd15ea
josoavj <josoavonjiniaina13@gmail.com>
2025-08-11 21:47:31 +0300

    feat: Script de suppression de la base de données

---
+++ Database/Script/Suppression.sql
@@ -0,0 +1,12 @@
+USE Prospectius;
+
+/*
+ SUPPRESSION DE TABLE SPECIFIQUE
+*/
+DROP TABLE Compte;
+DROP TABLE Administrateur;
+DROP TABLE Prospect;
+DROP TABLE HISTORIQUE;
+
+/* Suppression de la base de données */
+DROP DATABASE Prospectius;
